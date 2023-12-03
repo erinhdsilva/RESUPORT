3fe74376bf1132547be800b9c805aa38d3b159f3
erinhdsilva <erinhdsilva@gmail.com>
2023-12-04 02:40:46 +0530

    Aditya's files

---
+++ index.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    alert('Welcome to the login page');
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    console.log('Logging in with:', username, password);
+}
